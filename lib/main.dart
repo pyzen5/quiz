bd97b890df3c57d857034186e894a055f0e9b79b
pyzen5 <pyzen55@gmail.com>
2020-02-12 16:28:37 +0530

    fun

--- lib/main.dart
+++ lib/main.dart
@@ -21,34 +21,41 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': 'Whats your favorite color?',
       'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 50},
-        {'text': 'Blue', 'score': 60},
-        {'text': 'Green', 'score': 80},
+        {'text': 'Black', 'score': 1},
+        {'text': 'Red', 'score': 2},
+        {'text': 'Blue', 'score': 3},
+        {'text': 'Green', 'score': 4},
       ],
     },
     {
       'questionText': 'Whats your favorite animal?',
       'answers': [
-        {'text': 'Rabbit', 'score': 85},
-        {'text': 'Snake', 'score': 50},
-        {'text': 'Lion', 'score': 60},
-        {'text': 'Elephant', 'score': 70},
+        {'text': 'Rabbit', 'score': 1},
+        {'text': 'Snake', 'score': 3},
+        {'text': 'Lion', 'score': 4},
+        {'text': 'Elephant', 'score': 2},
       ],
     },
     {
       'questionText': 'Whos your favorite instructor?',
       'answers': [
-        {'text': 'John', 'score': 60},
-        {'text': 'Dew', 'score': 50},
-        {'text': 'Doe', 'score': 20},
-        {'text': 'Wick', 'score': 100},
+        {'text': 'John', 'score': 2},
+        {'text': 'Dew', 'score': 3},
+        {'text': 'Doe', 'score': 4},
+        {'text': 'Wick', 'score': 1},
       ],
     },
   ];
 
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
 
@@ -76,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                 questions: _questions,
                 questionIndex: _questionIndex,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,30 +2,53 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetQuiz;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetQuiz);
 
   String get resultPhrase {
     var resultText = 'You did it!!';
-    if(resultScore <= 50) {
-      resultText = 'You are awesome';
-    } else if(resultScore <= 70) {
-      resultText = 'You are Likeable';
-    } else if(resultScore <= 100) {
-      resultText = 'You are Bad';
-    } else {
-      resultText = 'You are What you are!!';
+    switch (resultScore) {
+      case 4: resultText = 'You are Unique!!'; break;
+      case 5: resultText = 'You are Awesome!!'; break;
+      case 6: resultText = 'You are Fantastic!!'; break;
+      case 7: resultText = 'You are Cool!!'; break;
+      case 8: resultText = 'You are Wonderful!!'; break;
+      case 9: resultText = 'You are Something!!'; break;
+      case 10: resultText = 'You are Nothing!!'; break;
+      case 11: resultText = 'You are God!!'; break;
+      case 12: resultText = 'You are Bad!!'; break;
+      case 3: resultText = 'You are Evil!!'; break;      
+      default: resultText = 'You are What you are!!';
     }
+    // if (resultScore <= 50) {
+    //   resultText = 'You are awesome';
+    // } else if (resultScore <= 70) {
+    //   resultText = 'You are Likeable';
+    // } else if (resultScore <= 100) {
+    //   resultText = 'You are Bad';
+    // } else {
+    //   resultText = 'You are What you are!!';
+    // }
     return resultText;
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetQuiz,
+            color: Colors.blue,
+            child: Text('Restart Quiz!!'),
+          ),
+        ],
       ),
     );
   }
