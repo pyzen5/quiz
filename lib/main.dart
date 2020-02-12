f2c1f8cc4ee5b0e944c9765b7bf03aeabaf49325
pyzen5 <pyzen55@gmail.com>
2020-02-12 15:00:48 +0530

    dynamic answer

--- lib/main.dart
+++ lib/main.dart
@@ -22,16 +22,25 @@ class _MyAppState extends State<MyApp> {
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
-    });    
+    });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    
     var questions = [
-      'Whats your favorite color?',
-      'Whats your favorite animal?'
+      {
+        'questionText': 'Whats your favorite color?',
+        'answers': ['Black', 'Red', 'Blue', 'Green'],
+      },
+      {
+        'questionText': 'Whats your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Lion', 'Elephant'],
+      },
+      {
+        'questionText': 'Whos your favorite instructor?',
+        'answers': ['John', 'Dew', 'Doe', 'Wick'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -40,10 +49,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
           ],
         ),
       ),
